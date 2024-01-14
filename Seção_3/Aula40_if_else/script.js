cc99e303517ddb6ada98687e655a4f23bf2ed04a
Fernando Felinto <103469246+FernandoFelinto@users.noreply.github.com>
2024-01-14 15:05:22 -0300

    Add files via upload

---
+++ Seção_3/Aula40_if_else/script.js
@@ -0,0 +1,12 @@
+const hora = 5;
+
+if (hora >= 0 && hora < 12) {
+    console.log('Bom dia!');
+}else if (hora >= 12 && hora <= 18) {
+    console.log('Boa Tarde!');
+}else if(hora > 18 && hora < 24){
+    console.log('Boa Noite!');
+}else{
+    console.log('[ERRO] Hora Inválida!');
+}
+//console.log(hora);
